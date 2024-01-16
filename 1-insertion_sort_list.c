e070b9dfb7da69056bfbb9cd0368fd912cf08d03
Rashadmlkv <resadmlkv@gmail.com>
2024-01-16 02:30:03 -0800

    Desired output was swapping element in each backwards, mine did on stopping at bigger elemnt

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,42 +1,45 @@
 #include "sort.h"
 
+
 /**
- * insertion_sort_list - insertion sort algortithm
- * @list: linked list
+ * insertion_sort_list - sorts a doubly linked list
+ * order using the Insertion sort algorithm
+ * @list: double pointer to the head of the list
+ *
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *tmp = *list, *swap, *tmp2;
+	listint_t *curr, *next, *prev;
 
-	for (; tmp != NULL; tmp = tmp->next)
+	if ((!*list) || (!(*list)->next))
+		return;
+
+	curr = (*list)->next;
+	while (curr)
 	{
-		if (tmp->prev != NULL && (tmp->n < tmp->prev->n))
+		next = curr->next;
+
+		while (curr->prev != NULL && curr->prev->n > curr->n)
 		{
-			swap = tmp;
-			tmp = tmp->prev;
-			tmp->next = swap->next;
-                        if (tmp->next)
-                                tmp->next->prev = tmp;
+			prev = curr->prev;
+			if (curr->next != NULL)
+				curr->next->prev = prev;
 
-			tmp2 = tmp;
-			for (; tmp2->prev && (swap->n < tmp2->n); tmp2 = tmp2->prev)
-				;
+			prev->next = curr->next;
+			curr->next = prev;
+			curr->prev = prev->prev;
 
-			if (!tmp2->prev)
-			{
-				swap->prev = tmp2->prev;
-				swap->next = tmp2;
-				tmp2->prev = swap;
-				*list = swap;
-			}
-			else
-			{
-				swap->prev = tmp2;
-				swap->next = tmp2->next;
-				tmp2->next = swap;
-				swap->next->prev = swap;
-			}
+			if (prev->prev != NULL)
+				prev->prev->next = curr;
+
+			prev->prev = curr;
+
+			if (curr->prev == NULL)
+				*list = curr;
+
 			print_list(*list);
 		}
+
+		curr = next;
 	}
 }
